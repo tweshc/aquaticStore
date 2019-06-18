7ecc204bb2c10ff22c49f78e42e74d79d138231b
Twesh <twesh.chowdhury@mastercard.com>
2019-06-18 14:28:39 -0400

    new pojo

--- src/main/java/com/inrhythm/aquaticStore/model/entity/ShoppingCart.java
+++ src/main/java/com/inrhythm/aquaticStore/model/entity/ShoppingCart.java
@@ -1,4 +1,13 @@
 package com.inrhythm.aquaticStore.model.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
 public class ShoppingCart {
+    List<Product> productsInCart = new ArrayList<>();
 }
