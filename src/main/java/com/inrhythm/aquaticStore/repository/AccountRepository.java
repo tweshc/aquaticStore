f835f886453d4b2ff77dcb6e1968ad540b11ccce
Twesh <tweshc@gmail.com>
2019-07-01 20:26:38 -0400

    deleted useless models and repository

--- src/main/java/com/inrhythm/aquaticStore/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.inrhythm.aquaticStore.repository;
-
-import com.inrhythm.aquaticStore.model.entity.Account;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AccountRepository extends MongoRepository<Account, String> {
-
-    Account findByUserName(String userName);
-}
